f7062459523085ab10485fecf6261b2df421f23b
Matt Ray <matthewhray@gmail.com>
2013-01-16 10:31:34 -0600

    added support for banners, getting plugin names and versions and merging options.

--- lib/sputnik/plugin.rb
+++ lib/sputnik/plugin.rb
@@ -4,9 +4,34 @@ module Sputnik
   class Plugin
     include Mixlib::CLI
 
+    #this should be overridden
+    banner('Usage: sputnik [plugin] [options]')
+
+    option :debug,
+    :long => '--debug',
+    :description => 'Enable debugging if available',
+    :boolean => true
+
+    option :version,
+    :short => '-v',
+    :long => '--version',
+    :description => 'Show Sputnik plugin version',
+    :boolean => true,
+    :proc => nil, #gets defined in the self.call
+    :exit => 0
+
     def self.call
-      plugin = new
+      parent = Sputnik::Plugin.new
+      plugin = self.new
+      name = plugin.class.name.split("::").last
+      parent.options[:version][:proc] = Proc.new { puts "Sputnik #{name}: #{plugin.version}" }
+      plugin.options.merge!(parent.options) #merge base options
+      plugin.parse_options
       plugin.call
+    end
+
+    def version
+      return "unknown"
     end
 
     def call
