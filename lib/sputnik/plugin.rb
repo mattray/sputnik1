dc9d0a119209c8655586381148eb72d94b15fc07
Matt Ray <matthewhray@gmail.com>
2013-01-16 09:03:04 -0600

    args are already available through parse_options, no need to duplicate effort.

--- lib/sputnik/plugin.rb
+++ lib/sputnik/plugin.rb
@@ -1,42 +1,17 @@
 require 'mixlib/cli'
 
 module Sputnik
-  DEBUG = false
   class Plugin
     include Mixlib::CLI
 
-    def self.call(*args)
+    def self.call
       plugin = new
-      plugin.parse_options args
       plugin.call
     end
 
-    def call(*args)
-
+    def call
+      puts "Sputnik::Plugin.call (YOU SHOULD DEFINE THIS!)"
     end
 
-    # option :debug,
-    #   :boolean => true,
-    #   :default => false,
-    #   :long => "--debug",
-    #   :description => "Show lots of Sputnik debugging output.",
-    #   :proc => Proc.new { |key| Sputnik::DEBUG = key }
-    #
-    # option :help,
-    #   :short => "-h",
-    #   :long => "--help",
-    #   :description => "Show this message",
-    #   :on => :tail,
-    #   :boolean => true,
-    #   :show_options => true,
-    #   :exit => 0
-    #
-    # option :version,
-    #   :short => "-v",
-    #   :long => "--version",
-    #   :description => "Show Test Kitchen version",
-    #   :boolean => true,
-    #   :proc => lambda {|v| puts "sputnik: #{::Sputnik::VERSION}"},
-    #   :exit => 0
   end
 end
